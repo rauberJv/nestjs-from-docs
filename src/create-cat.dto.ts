1a08c39b2f06e82762ea3141b862c23d60c23b0d
Luis Fernando Rauber <rauberjuvinel@gmail.com>
2022-08-26 00:32:49 -0300

    New Comments in create-cat.dto.ts

--- src/create-cat.dto.ts
+++ src/create-cat.dto.ts
@@ -2,6 +2,10 @@
     Uma DTO(Data Transfer Object) é muito utilizada no 
     TypeScript para definir o formato que os dados 
     são passados através da internet
+
+    O ideal talvez seja criar um DTO com nome mais 
+    generalizado para que possa ser utilizado mais 
+    de uma vez e não precisar repetir código
 */
 
 export class CreateCatDto {
